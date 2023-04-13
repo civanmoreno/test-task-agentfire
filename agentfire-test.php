625b33863a05c6556ae7d29f1d8047f223195e6d
Iván Moreno <carlosivanmorenoc@gmail.com>
2023-04-12 19:43:22 -0500

    Add bootstrap, mapbox and customs css nad js

--- agentfire-test.php
+++ agentfire-test.php
@@ -33,3 +33,24 @@ include( plugin_dir_path( __FILE__ ) . 'src/shortcodes/shortcodes.php');
 
 // Including Settings Page Config.
 include( plugin_dir_path( __FILE__ ) . 'src/admin/admin.php');
+
+// Add bootstrap and custom css.
+function agentfire_test_enqueue_styles() {
+    wp_enqueue_style( 'bootstrap', plugins_url( 'vendor/twbs/bootstrap/dist/css/bootstrap.min.css', __FILE__ ) , false, '1.0', 'all' ); // Inside a parent theme
+    wp_enqueue_style( 'agentfire-css', plugins_url('/assets/css/agentfire.css', __FILE__ ));
+    wp_enqueue_style( 'mapbox-css', 'https://api.mapbox.com/mapbox-gl-js/v2.14.0/mapbox-gl.css');
+}
+
+// Add boostrap js.
+function agentfire_test_enqueue_scripts() {
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap-js', plugins_url('vendor/twbs/bootstrap/dist/js/bootstrap.min.js', __FILE__),  array('jquery'), '5.3', true );
+
+    wp_enqueue_script( 'agentfire-js', plugins_url('/assets/js/agentfire.js', __FILE__), array(), '1.0.0', true );
+    wp_enqueue_script( 'mapbox-js', 'https://api.mapbox.com/mapbox-gl-js/v2.14.0/mapbox-gl.js', array(), '1.0.0', true );
+
+}
+
+// Add actions to enqueue scripst
+add_action( 'wp_enqueue_scripts', 'agentfire_test_enqueue_scripts', 20);
+add_action( 'wp_enqueue_scripts', 'agentfire_test_enqueue_styles', 20);
